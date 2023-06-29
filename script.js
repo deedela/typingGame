75fe1268c70f7258e6465db83eff2e7d37fa8da9
maria <mariadela@gmail.com>
2023-06-29 12:08:07 -0400

    project is working

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
 const quote = document.getElementById('quote');
 const input = document.getElementById ('typed-value');
 const start = document.getElementById('start');
+const message = document.getElementById('message');
+const quotes = [
+  'Things are only impossible until they are not',
+  'It is possible to commit no errors and still lose. That is not a weakness. That is life',
+  'There is a way out of every box, a solution to every puzzle; it is just a matter of finding it.',
+  'Without freedom of choice there is no creativity',
+  'Logic is the beginning of wisdom, not the end',
+  'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+  'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+];
 
-let quoteText;
 let wordQueue;
 let highLightPosition;
+let starTtime;
 
 function startGame() {
-    quoteText = 'type me';
+    // quoteText = 'type me';
+    console.log("Game started!");
+
+    const quoteIndex = Math.floor(Math.random()* quotes.length);
+    let quoteText = quotes[quoteIndex];
     wordQueue = quoteText.split(' ');
     quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');    
     
     highLightPosition = 0;
     // quote.childNodes = ['<span>type</span>', '<span>me</span>']
     quote.childNodes[highLightPosition].className = 'highlight';
-  }
+    
+    document.body.className = "";
+    start.className = "started";
+    message.innerHTML = "";
+  
+    startTime = new Date().getTime();
+    setTimeout(() => { start.className = "button"; }, 2000);
+}
+
 
 function checkInput() {
-    console.log('Checking', input.value);
-    const currentWord = wordQueue [0]; // the first word is the word queue array
-    const typedValue = input.value.trim(); //JS string method '  hello world  ' = 'hello world'
-
-    if (currentWord !== typedValue) { //false when the word typed is correct
-      input.className = currentWord.startsWith(typedValue) ? '' : 'error';
-      // if (currentWord.startsWith(typedValue) === true) {
-      //   input.className = '';
-      // } else {
-      //   input.className = 'error';
-      // }
-      return;
-    }
-
-    // this is what happens when there's no error 
-    wordQueue.shift(); //remove the first word from the wordQueue array, because we already typed it!
-    input.value = ''; //clear input field
+  const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", "");
+  const typedValue = input.value.trim();
+  
+  if (currentWord !== typedValue) {
+    input.className = currentWord.startsWith(typedValue) ? "" : "error";
+    return;
   }
+
+  wordQueue.shift(); // remove the first item from the array, making the next item our new `currentWord`
+  input.value = '';
+console.log (quote, highLightPosition)
+  quote.childNodes[highLightPosition].className = ""; // [0] == '<span>type</span>'
+
+  if (wordQueue.length === 0) {
+    gameOver();
+    return;
+  }
+
+  highLightPosition++;                      
+  quote.childNodes[highLightPosition].className = 'highlight'; // [1] == '<span>me</span>'
+}
+
+function gameOver() {
+  const elapsedTime = new Date().getTime() - startTime;
+  document.body.classname = "winner";
+  message.innerHTML = `<span class="congrats">Congratulations!</span><br />
+  You finished in ${elapsedTime / 1000} seconds`;
+}
+
+start.addEventListener('click', startGame);
+input.addEventListener('input', checkInput);
+
   
   start.addEventListener('click', startGame);
   input.addEventListener('input', checkInput);
