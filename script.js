ed1f4dd1011c72aa0c580f5fa674fc216025eb16
maria <mariadela@gmail.com>
2023-07-02 23:16:54 -0400

    some minor changes

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ const input = document.getElementById ('typed-value');
 const start = document.getElementById('start');
 const message = document.getElementById('message');
 const quotes = [
-  'Things are only impossible until they are not',
-  'It is possible to commit no errors and still lose. That is not a weakness. That is life',
-  'There is a way out of every box, a solution to every puzzle; it is just a matter of finding it.',
-  'Without freedom of choice there is no creativity',
-  'Logic is the beginning of wisdom, not the end',
-  'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
-  'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
+  'type me'
+  // 'Things are only impossible until they are not',
+  // 'It is possible to commit no errors and still lose. That is not a weakness. That is life',
+  // 'There is a way out of every box, a solution to every puzzle; it is just a matter of finding it.',
+  // 'Without freedom of choice there is no creativity',
+  // 'Logic is the beginning of wisdom, not the end',
+  // 'Improve a mechanical device and you may double productivity. But improve yourself, you gain a thousandfold',
+  // 'Compassion: that is the one thing no machine ever had. Maybe it is the one thing that keeps us ahead of them.',
 ];
 
 let wordQueue;
 let highLightPosition;
-let starTtime;
+let startTime;
 
 function startGame() {
     // quoteText = 'type me';
@@ -34,9 +35,16 @@ function startGame() {
     message.innerHTML = "";
   
     startTime = new Date().getTime();
-    setTimeout(() => { start.className = "button"; }, 2000);
-}
+    setTimeout(() => { 
+      start.className = "launch";
+    }, 500);  
+    setTimeout(() => {    
+      start.style.display = "none";
+    }, 750);
 
+    input.focus();
+
+}
 
 function checkInput() {
   const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", "");
@@ -63,9 +71,10 @@ console.log (quote, highLightPosition)
 
 function gameOver() {
   const elapsedTime = new Date().getTime() - startTime;
-  document.body.classname = "winner";
+  document.body.className = "winner";
   message.innerHTML = `<span class="congrats">Congratulations!</span><br />
   You finished in ${elapsedTime / 1000} seconds`;
+
 }
 
 start.addEventListener('click', startGame);
